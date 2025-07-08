2ec38f65b7cb75fdf3583af02fdb82bc41f72af8
meedumi-pa <R.meedu96@gmail.com>
2025-07-08 11:22:13 +0530

    Card components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,27 @@
-function Home(){
-    return <h1>Home</h1>
+import DefaultCard from "./cards/DefaultCard";
+
+function Home() {
+  return (
+    <>
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <DefaultCard />
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <DefaultCard />
+          </div>
+
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <DefaultCard />
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <DefaultCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Home;
